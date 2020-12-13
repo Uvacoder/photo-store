2901771d4f8c623d156b6aa41557e3ebac48f65d
Cameron Fyfe <cameron.j.fyfe@gmail.com>
2020-12-12 18:01:02 -0700

    add subitems to sidebar

--- src/components/SidebarMenu/sidebar-components/SidebarItem.js
+++ src/components/SidebarMenu/sidebar-components/SidebarItem.js
@@ -1,16 +1,42 @@
+import React from 'react';
 import './SidebarItem.css';
 import { NavLink } from 'react-router-dom';
 
 const SidebarItem = ({
-  className,
   children,
-  to
-}) => (
-  <div className={className}>
-    <NavLink activeClassName='active' to={to}>
-      {children}
-    </NavLink>
-  </div>
-);
+  className,
+  isMobile,
+  to,
+  onPick,
+  isSubHeader=false,
+  isSubOpen=false,
+  content
+}) => {
+
+  const cl = isMobile
+    ? "sidebar-item-container sic-mobile"
+    : "sidebar-item-container"
+  ;
+  const clSubOpen = cl + " " + "sidebar-item-sub-header";
+  const clSub = cl + " " + "sidebar-subitem-container" + (isSubOpen ? "" : " "+"sidebar-subitem-hidden");
+
+  const clName = className || ((isSubHeader&&isSubOpen) ? clSubOpen : cl);
+
+  return (
+    <div className={clName}>
+      <NavLink activeClassName='active' to={to} onClick={() => onPick?.(to)}>
+        {content}
+      </NavLink>
+      {children &&
+        React.Children.toArray(children).map(c => React.cloneElement(c, {
+          className: clSub,
+          isMobile: isMobile,
+          isSubOpen: isSubOpen,
+          onPick: onPick
+        }))
+      }
+    </div>
+  );
+}
 
 export default SidebarItem;
