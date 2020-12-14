e0c78f91566cbc022ea966a247448054bc969c9f
Cameron Fyfe <cameron.j.fyfe@gmail.com>
2020-12-14 12:07:25 -0700

    update home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,13 @@
 import './Home.css';
 
 const Home = () => (
-  <div className="home-border">
-    <div className="home-div">
-      <div className="space" />
-      <div className="content">
-        <h3 style={{textAlign: "center"}}>.:: Atelier Mistral ::.</h3>
-        <h3 style={{textAlign: "center"}}>Home</h3>
-      </div>
-      <div className="space" />
-      <div className="content">More stuff</div>
-      <div className="content">More stuff</div>
-      <div className="content">More stuff</div>
-      <div className="content">More stuff</div>
+  <div className="home-grid">
+    <div className="atelier-mistral">
+      <p className="title">Atelier Mistral</p>
+      <p className="pronunciation">ah-tuh-lyae · mis-trahl</p>
+      <p className="desc">
+        a strong, northwesterly wind that blows through the Rhône valley and southern France into the Mediterranean
+      </p>
     </div>
   </div>
 );
