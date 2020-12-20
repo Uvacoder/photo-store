878beb998a4c35bb2e8d73fc0780f6bd4d39ff36
Cameron Fyfe <cameron.j.fyfe@gmail.com>
2020-12-20 12:24:06 -0700

    restyle with styled-components

--- src/components/BurgerIcon/BurgerIcon.js
+++ src/components/BurgerIcon/BurgerIcon.js
@@ -1,13 +1,27 @@
-import './BurgerIcon.css';
+import styled from 'styled-components';
+
+const Bar = styled.div`
+  width: 30px;
+  height: 4px;
+  border-radius: 2px;
+  background-color: var(--social-media-icon-link-color);
+  margin: 5px 0;
+  transition: 0.4s;
+  ${props => props.asXBar1 && '-webkit-transform: translate(0px, 9px) rotate(-45deg)'};
+  ${props => props.asXBar1 && 'transform: translate(0px, 9px) rotate(-45deg)'};
+  ${props => props.asXBar2 && 'opacity: 0'};
+  ${props => props.asXBar3 && '-webkit-transform: translate(0px, -9px) rotate(45deg)'};
+  ${props => props.asXBar3 && 'transform: translate(0px, -9px) rotate(45deg)'};
+`;
 
 const BurgerIcon = ({
   isOpen,
   onClick
 }) => (
-  <div className={isOpen ? "change-to-x" : ""} onClick={onClick}>
-    <div className="bar1"></div>
-    <div className="bar2"></div>
-    <div className="bar3"></div>
+  <div onClick={onClick}>
+    <Bar asXBar1={isOpen} />
+    <Bar asXBar2={isOpen} />
+    <Bar asXBar3={isOpen} />
   </div>
 );
 
