6cce75fef67f59f3b775c1488ace6e994bcadb8e
Cameron Fyfe <cameron.j.fyfe@gmail.com>
2020-12-20 13:03:21 -0700

    restyle XButton as styled-component

--- src/components/XButton/XButton.js
+++ src/components/XButton/XButton.js
@@ -1,4 +1,41 @@
-import './XButton.css';
+import styled from 'styled-components';
+
+const XButtonCircle = styled.div`
+  height: 100%;
+  width: 100%;
+  border-radius: 50%;
+  background-color: white;
+  opacity: 0.6;
+  transition: opacity 0.2s;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+const X = styled.div`
+  position: absolute;
+  left: 14%;
+  width: 70%;
+  top: 44%;
+  height: 12%;
+  background-color: #3f3f3f;
+  opacity: 1;
+  transition: background-color 0.2;
+  &:hover {
+    background-color: black;
+  }
+`;
+
+const X1 = styled(X)`
+  -webkit-transform: rotate(-45deg);
+  transform: rotate(-45deg);
+`;
+
+const X2 = styled(X)`
+  -webkit-transform: rotate(45deg);
+  transform: rotate(45deg);
+`;
 
 const XButton = ({
   className,
@@ -8,13 +45,12 @@ const XButton = ({
 }) => (
   <div className={className} onClick={onClick}>
     <div style={{ position: "relative", width, height }}>
-      <div className="x-btn-circle" >
-        <div className="x x-1" />
-        <div className="x x-2" />
-      </div>
+      <XButtonCircle>
+        <X1/>
+        <X2/>
+      </XButtonCircle>
     </div>
   </div>
-
 )
 
 export default XButton;
